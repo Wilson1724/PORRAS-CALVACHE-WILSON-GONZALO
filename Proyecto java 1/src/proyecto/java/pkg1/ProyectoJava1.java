2c43b0b74a18900fd618c8654c1d1dd13ccc4a81
Wilson1724 <61748173+Wilson1724@users.noreply.github.com>
2020-03-10 11:08:18 -0500

    tipo de datos

--- Proyecto java 1/src/proyecto/java/pkg1/ProyectoJava1.java
+++ Proyecto java 1/src/proyecto/java/pkg1/ProyectoJava1.java
@@ -17,5 +17,16 @@ public class ProyectoJava1 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    //comentario de una linea//
     
+    //DECLARAR VARIABLES//
+    int numeroEntero = 5;
+    double numeroDouble = 3.14;
+    char letra = 'A';
+    String cadena = "hola mundo";
+    boolean valorBoolean = false;
+    boolean valorBool = true;
+    /*solo numeros que representan las distintas fnciones de la ecuacion
+    dada en el libro de la materia vista
+    */
 }
